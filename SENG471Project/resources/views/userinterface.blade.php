8b68a6a4aa865b85fe38fb74c01a8a9a28b2a7b0
ali_hassam <alykhanhssm@gmail.com>
2019-04-07 15:56:22 -0600

    added some javascipt

--- SENG471Project/resources/views/userinterface.blade.php
+++ SENG471Project/resources/views/userinterface.blade.php
@@ -24,6 +24,22 @@
 
             {{csrf_field()}}
 
+            <script type="text/javascript">
+
+            function setForm(value) {
+
+            var vehicle = document.getElementsByName("Vehicles")[0].value;
+
+
+            if(value == 'form1' && vehicle == "Sedan")
+            {
+                document.getElementById('form1').style='display:block;';
+            }
+
+}
+
+            </script>
+
             <div>
               Select Vehicle:<br>
               <select name="Vehicles">
@@ -43,8 +59,27 @@
                 <option value="#999999">Grey</option>
               </select>
             </div>
+
+            <div>
+            <label>Include Interior View</label>
+            <select id="select1" onchange="setForm(this.value)">
+            <option value="form2">No</option>
+            <option value="form1">Yes</option>
+            </select>
+            </div>
+
+            <div id="form1" style="display: none">
+            Select Interior Color:<br>
+              <select name="Colors">
+                <option value="#ff0000">Dark Grey</option>
+                <option value="#0000ff">Jet Black</option>
+                <option value="#ffffff">Dark Wheat</option>
+              </select>
+            </div>
+
+
             
-              <input type="submit" value="Submit">
+            <input type="submit" value="Submit">
             </form>
 
             <p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
@@ -52,11 +87,13 @@
             @if (isset($data))
 
 
-            @if($data['firstname'] == 'Mickey')
+            @if($data['Vehicles'] == 'Sedan')
 
-            <p> Made it here! </p>
+            <p> We are in Sedan </p>
+
             @endif
 
+            <p>{{dd($data)}}</p>
 
 
             @endif

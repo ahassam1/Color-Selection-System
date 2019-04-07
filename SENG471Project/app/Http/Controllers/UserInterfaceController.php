abd99da0d62c3dddd27ff2c88ca3a29befbf8cf6
ali_hassam <alykhanhssm@gmail.com>
2019-04-07 13:09:49 -0600

    jh

---
+++ SENG471Project/app/Http/Controllers/UserInterfaceController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserInterfaceController extends Controller
+{
+
+    public function index()
+    {
+        return view('userinterface');
+    }
+}
